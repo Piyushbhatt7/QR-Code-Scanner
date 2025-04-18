a9b473c8d47c172a030ddc8083e8eca90b01af08
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-18 22:47:44 +0530

    scan QR Code

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class GenerateQrCode extends StatelessWidget {
+class GenerateQrCode extends StatefulWidget {
   const GenerateQrCode({super.key});
 
+  @override
+  State<GenerateQrCode> createState() => _GenerateQrCodeState();
+}
+
+class _GenerateQrCodeState extends State<GenerateQrCode> {
   @override
   Widget build(BuildContext context) {
     return Container();
