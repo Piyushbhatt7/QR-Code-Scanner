fc85fe55049e5097d3401cb4f9e335bfac9e51b8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-19 13:35:04 +0530

    scanning qr code

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanQrCode extends StatefulWidget {
